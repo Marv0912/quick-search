90c8a0c9ccbbc72d177e83860ccd3a7a6ff9de7a
Marv0912 <marvin.fajardo.s@icloud.com>
2023-10-23 23:03:39 -0400

    create quick search for google chrome cd /Users/marvinfajardo/Documents/Programming/quick-search ; /usr/bin/env /opt/homebrew/bin/python3 /Users/marvinfajardo/.vscode/extensions/ms-python.python-2023.14.0/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 50472 -- /Users/marvinfajardo/Documents/Programming/quick-search/quick_search.py

---
+++ quick_search.py
@@ -0,0 +1,18 @@
+import argparse
+import subprocess
+import urllib.parse
+
+def search(query):
+    search_query = urllib.parse.quote(query)
+    url = f"https://google.com/search?q={search_query}"
+    subprocess.run(["google-chrome", url])
+
+def main():
+    parser = argparse.ArgumentParser(description="Quick terminal search using Google Chrome")
+    parser.add_argument("query", type=str, help="Search query")
+
+    args = parser.parse_args()
+    search(args.query)
+
+if __name__ == "__main__":
+    main()
